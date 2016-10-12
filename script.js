a2866d1ea58df2897f16b5df37d4a77231bf465e
Micah Boyd <micahhope@yahoo.com>
2016-10-12 12:53:54 +1100

    got it working with <h2> tags around X, O

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ var xo = function(){ // Fuction Adds 'X' or 'O' to grid
     && $(event.target).text() != 'X'
     && $(event.target).text() != 'O') {
 
-    $(event.target).text('X');
+    $(event.target).html('<h2>X</h2>');
     playerName = 'P2'
   }
 
   else if (playerName === 'P2'
     && $(event.target).text() != 'X'
     && $(event.target).text() != 'O') {
-    $(event.target).text('O');
+    $(event.target).html('<h2>O</h2>');
     playerName = 'P1'
   }
 
@@ -33,24 +33,26 @@ var playerNameChange = function(){ // Changes player turn
 var checkWinner = function(){ // Checks if 'X' or '0' are     placed in 3 a row, declares winner
 
   var combinations = [
-    [$('#1').text(), $('#2').text(), $('#3').text()],
-    [$('#4').text(), $('#5').text(), $('#6').text()],
-    [$('#7').text(), $('#8').text(), $('#9').text()],
-    [$('#1').text(), $('#4').text(), $('#7').text()],
-    [$('#2').text(), $('#5').text(), $('#8').text()],
-    [$('#3').text(), $('#6').text(), $('#9').text()],
-    [$('#1').text(), $('#5').text(), $('#9').text()],
-    [$('#3').text(), $('#5').text(), $('#7').text()],
+    [$('#s1').text(), $('#s2').text(), $('#s3').text()],
+    [$('#s4').text(), $('#s5').text(), $('#s6').text()],
+    [$('#s7').text(), $('#s8').text(), $('#s9').text()],
+    [$('#s1').text(), $('#s4').text(), $('#s7').text()],
+    [$('#s2').text(), $('#s5').text(), $('#s8').text()],
+    [$('#s3').text(), $('#s6').text(), $('#s9').text()],
+    [$('#s1').text(), $('#s5').text(), $('#s9').text()],
+    [$('#s3').text(), $('#s5').text(), $('#s7').text()],
   ];
 
   for (var i = 0; i < combinations.length; i++) {
 
-    if (combinations[i].join('') === 'XXX') {
+    if (combinations[i].join('').replace(/<h2>/g, '')
+      .replace('/', '') === 'XXX') {
       $('.player-name').text("Player One Wins!");
       playerName = null;
     }
 
-    if (combinations[i].join('') === 'OOO') {
+    if (combinations[i].join('').replace(/<h2>/g, '')
+      .replace('/', '') === 'OOO') {
       $('.player-name').text("Player Two Wins!");
       playerName = null;
     }
